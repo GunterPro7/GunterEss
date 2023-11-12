2d1b4943970dd25de00f540c0ed66b581d5f77b7
GunterPro7 <florianrauscher700@gmail.com>
2023-11-12 16:16:20 +0100

    fixed the auto updater

--- src/main/java/com/GunterPro7uDerKatzenLord/Test.java
+++
@@ -1,9 +0,0 @@
-package com.GunterPro7uDerKatzenLord;
-
-import javax.swing.*;
-
-public class Test extends JFrame {
-    public static void main(String[] args) {
-        new JFrame().setVisible(true);
-    }
-}
