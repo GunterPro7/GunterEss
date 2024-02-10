1249cdf741ad65c705ae2a4b6ce00d35469b48d9
GunterPro7 <florianrauscher700@gmail.com>
2024-02-10 01:26:51 +0100

    bugFixes

--- src/main/java/com/GunterPro7uDerKatzenLord/Listener/QuiverEmptyChecker.java
+++ src/main/java/com/GunterPro7uDerKatzenLord/Listener/QuiverEmptyChecker.java
@@ -3,22 +3,30 @@ package com.GunterPro7uDerKatzenLord.Listener;
 import com.GunterPro7uDerKatzenLord.Gui.CustomIngameUI;
 import net.minecraft.client.Minecraft;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Items;
 import net.minecraft.inventory.Container;
 import net.minecraft.inventory.ContainerChest;
+import net.minecraft.item.ItemBow;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.nbt.NBTTagList;
 import net.minecraft.util.ChatComponentText;
 import net.minecraft.util.IChatComponent;
+import net.minecraftforge.client.event.ClientChatReceivedEvent;
 import net.minecraftforge.client.event.MouseEvent;
 import net.minecraftforge.event.entity.player.PlayerOpenContainerEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent;
 import net.minecraftforge.fml.relauncher.Side;
 
+import java.util.Random;
+
 public class QuiverEmptyChecker {
     private static final QuiverEmptyChecker INSTANCE = new QuiverEmptyChecker();
 
     private int arrows;
     private boolean loadedArrows;
+    private static final Random random = new Random();
 
     private QuiverEmptyChecker() {
 
@@ -35,7 +43,16 @@ public class QuiverEmptyChecker {
     }
 
     public void reload(Container container) {
+        arrows = 0;
+        loadedArrows = true;
 
+        container.inventorySlots.forEach(slot -> arrows += slot.getStack().stackSize);
+    }
+
+    @SubscribeEvent
+    public void onChatMessage(ClientChatReceivedEvent event) {
+        // TODO if matches with something like this: You filled your quiver with 2,816 extra arrows!
+        // TODO aktuellisieren des quiver coints
     }
 
     @SubscribeEvent
@@ -44,28 +61,56 @@ public class QuiverEmptyChecker {
             if (Minecraft.getMinecraft().thePlayer.openContainer != null) {
                 Container container = Minecraft.getMinecraft().thePlayer.openContainer;
 
-                IChatComponent displayName = ((ContainerChest) container).getLowerChestInventory().getDisplayName();
-                AdvancedChat.sendPrivateMessage(displayName.getFormattedText());
+                if (container instanceof ContainerChest) {
+                    IChatComponent displayName = ((ContainerChest) container).getLowerChestInventory().getDisplayName();
+                    if (displayName.getFormattedText().equals("Quiver")) {
+                        reload(container);
+                    }
+                }
             }
         }
     }
 
     @SubscribeEvent
     public void onMouseEvent(MouseEvent event) {
-        // Check if the event is on the client side
         if (event.button == 0) {
-            // Get the current player
             EntityPlayer player = Minecraft.getMinecraft().thePlayer;
-            // Get the item in the player's hand
             ItemStack item = player.getHeldItem();
-            // Check if the button was pressed or released
-            if (event.buttonstate) {
-                // Button was pressed, do something
-                player.addChatMessage(new ChatComponentText("You pressed the left mouse button with " + item.getDisplayName()));
-            } else {
-                // Button was released, do something else
-                player.addChatMessage(new ChatComponentText("You released the left mouse button with " + item.getDisplayName()));
+
+            if (item != null && item.getItem() instanceof ItemBow) {
+                NBTTagCompound nbt = item.getTagCompound();
+                int infiniteQuiverLvl = 0;
+
+                if (nbt != null && nbt.hasKey("display", 10)) {
+                    NBTTagCompound display = nbt.getCompoundTag("display");
+
+                    if (display.hasKey("Lore", 9)) {
+                        NBTTagList lore = display.getTagList("Lore", 8);
+
+                        for (int i = 0; i < lore.tagCount(); i++) { // TODO nur wenn man einen pfeil weggschießt
+                            String loreString = lore.getStringTagAt(i);
+
+                            if (loreString.toLowerCase().contains("infinite quiver")) {
+                                try {
+                                    infiniteQuiverLvl = Integer.parseInt(loreString.toLowerCase().split("infinite quiver")[1].split(" ")[1]);
+                                } catch (NumberFormatException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        }
+                    }
+                }
+
+                if (random.nextInt(100) > infiniteQuiverLvl * 3) {
+                    arrowUse();
+                }
             }
         }
+    }
+
+    private void arrowUse() {
+        arrows--;
+
+        AdvancedChat.sendPrivateMessage("Only " + arrows + " Arrows Left!");
     }
 }
