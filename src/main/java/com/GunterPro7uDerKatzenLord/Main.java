42697c4be6f808215340753cef599a81c840f653
GunterPro7 <florianrauscher700@gmail.com>
2024-03-31 13:47:24 +0200

    implemented new auto updater

--- src/main/java/com/GunterPro7uDerKatzenLord/Main.java
+++ src/main/java/com/GunterPro7uDerKatzenLord/Main.java
@@ -33,7 +33,7 @@ public class Main {
     public static final Minecraft mc = Minecraft.getMinecraft();
     public static boolean starting = true;
     public static final String VERSION = "1.3.0";
-    public static final boolean DEV = true;
+    public static final boolean DEV = false;
     public static File configDirectory;
     public boolean updateAvailable;
 
