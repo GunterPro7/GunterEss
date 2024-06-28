547d67f63687bf1e667419e76475babcaf49c9c5
GunterPro7 <florianrauscher700@gmail.com>
2024-06-28 18:44:46 +0200

    v 1.4.3

--- src/main/java/com/GunterPro7/Main.java
+++ src/main/java/com/GunterPro7/Main.java
@@ -40,7 +40,7 @@ import static com.GunterPro7.listener.MiscListener.collectionJson;
 @Mod(modid = Main.MODID, useMetadata = true, version = Main.VERSION, guiFactory = "com.GunterPro7.GunterEssGuiFactory")
 public class Main {
     public static final String MODID = "GunterEss";
-    public static final String VERSION = "1.4.2";
+    public static final String VERSION = "1.4.3";
     public static final boolean DEV = false;
     public static final Minecraft mc = Minecraft.getMinecraft();
     public static final ScaledResolution scaledResolution = new ScaledResolution(mc);
